97f4f1091280ef7b0da38b68c08b8efa5b5e9730
Garrett Clary <claryg0603@protonmail.com>
2018-11-14 22:00:16 -0800

    removed commented-out lines

--- pymap.py
+++ pymap.py
@@ -11,15 +11,10 @@ def usage():
 def main():
     target = sys.argv[1] 
     port_range = sys.argv[2]
-
     port_scanner = nmap.PortScanner() # create port_scanner
-
+    
     port_scanner.scan(target, port_range) # pass the target IP and port range to scan
 
-    #print(port_scanner.scaninfo())
-    #print(port_scanner.all_hosts())
-    #print(port_scanner[target]['tcp'][int(port_range)])
-   
     print('Running command: %s\n' % (port_scanner.command_line())) # show the command line version of the scan
     
     for live_host in port_scanner.all_hosts():
